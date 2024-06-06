b1d5e61d0f645e54aae1037ee76a1010312c51eb
Shamanbenny <E1186165@u.nus.edu>
2024-06-06 10:09:20 +0800

    Updated & Organized Problem 5: Longest Palindromic Substring

--- 5_Longest Palindromic Substring/solution.py
+++ 5_Longest Palindromic Substring/solution.py
@@ -1,28 +1,35 @@
 # Solution Info
-# Runtime: 61 ms, faster than 99.10% of Python3 submissions.
-# Memory: 16.74 MB, less than 28.49% of Python3 submissions.
+# Runtime: 60 ms, faster than 99.12% of Python3 submissions.
+# Memory: 16.66 MB, less than 46.81% of Python3 submissions.
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        s_len = len(s)
         highest = 0
+        s_len = len(s)
         highest_str = ""
+
+        def checkPalindrome(
+            s: str, start_left: int, start_right: int, start_count: int
+        ) -> str:
+            i = 1
+            count = start_count
+            while (start_left - i) >= 0 and (start_right + i) < s_len:
+                if s[start_left - i] == s[start_right + i]:
+                    count += 2
+                    i += 1
+                else:
+                    break
+            i -= 1
+            return s[start_left - i : start_right + i + 1]
+
         low = (s_len // 2) - 1
         high = s_len // 2
         if s_len % 2 == 1:
             # Odd Length
             idx = s_len // 2
-            i = 1
-            count = 1
-            while (idx - i) >= 0:
-                if s[idx - i] == s[idx + i]:
-                    count += 2
-                    i += 1
-                else:
-                    break
-            if count > highest:
-                highest = count
-                i -= 1
-                highest_str = s[idx - i : idx + i + 1]
+            result = checkPalindrome(s, idx, idx, 1)
+            if len(result) > highest:
+                highest = len(result)
+                highest_str = result
             high += 1
         # Even Length
         iteration_left = low  # aka Remaining Iteration
@@ -31,96 +38,48 @@ class Solution:
             # Therefore, if highest >= 2(iteration_left), no need check for larger palindromic substring
 
             # [LEFT]
-            i = 1
-            count = 1
-            while (low - i) >= 0:
-                if s[low - i] == s[low + i]:
-                    count += 2
-                    i += 1
-                else:
-                    break
-            if count > highest:
-                highest = count
-                i -= 1
-                highest_str = s[low - i : low + i + 1]
+            result = checkPalindrome(s, low, low, 1)
+            if len(result) > highest:
+                highest = len(result)
+                highest_str = result
 
             # [LEFT] Even Palindrome
             if low != 0:
                 if s[low] == s[low - 1]:
                     # Left
-                    i = 1
-                    count = 2
-                    while ((low - 1 - i) >= 0) and ((low + i) < s_len):
-                        if s[low - 1 - i] == s[low + i]:
-                            count += 2
-                            i += 1
-                        else:
-                            break
-                    if count > highest:
-                        highest = count
-                        i -= 1
-                        highest_str = s[low - 1 - i : low + i + 1]
+                    result = checkPalindrome(s, low - 1, low, 2)
+                    if len(result) > highest:
+                        highest = len(result)
+                        highest_str = result
             if s[low] == s[low + 1]:
                 # Right
-                i = 1
-                count = 2
-                while ((low - i) >= 0) and ((low + 1 + i) < s_len):
-                    if s[low - i] == s[low + 1 + i]:
-                        count += 2
-                        i += 1
-                    else:
-                        break
-                if count > highest:
-                    highest = count
-                    i -= 1
-                    highest_str = s[low - i : low + i + 2]
+                result = checkPalindrome(s, low, low + 1, 2)
+                if len(result) > highest:
+                    highest = len(result)
+                    highest_str = result
 
             low -= 1
 
             # [RIGHT]
-            i = 1
-            count = 1
-            while (high + i) < s_len:
-                if s[high - i] == s[high + i]:
-                    count += 2
-                    i += 1
-                else:
-                    break
-            if count > highest:
-                highest = count
-                i -= 1
-                highest_str = s[high - i : high + i + 1]
+            result = checkPalindrome(s, high, high, 1)
+            if len(result) > highest:
+                highest = len(result)
+                highest_str = result
 
             # [RIGHT] Even Palindrome
             if high != s_len - 1:
                 if s[high] == s[high + 1]:
                     # Right
-                    i = 1
-                    count = 2
-                    while ((high - i) >= 0) and ((high + 1 + i) < s_len):
-                        if s[high - i] == s[high + 1 + i]:
-                            count += 2
-                            i += 1
-                        else:
-                            break
-                    if count > highest:
-                        highest = count
-                        i -= 1
-                        highest_str = s[high - i : high + i + 2]
+                    result = checkPalindrome(s, high, high - 1, 2)
+                    if len(result) > highest:
+                        highest = len(result)
+                        highest_str = result
             if s[high] == s[high - 1]:
                 # Left
-                i = 1
-                count = 2
-                while ((high - 1 - i) >= 0) and ((high + i) < s_len):
-                    if s[high - 1 - i] == s[high + i]:
-                        count += 2
-                        i += 1
-                    else:
-                        break
-                if count > highest:
-                    highest = count
-                    i -= 1
-                    highest_str = s[high - 1 - i : high + i + 1]
+                result = checkPalindrome(s, high - 1, high, 2)
+                if len(result) > highest:
+                    highest = len(result)
+                    highest_str = result
 
             high += 1
 
