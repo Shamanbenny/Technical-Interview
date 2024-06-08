49d1c49f5a8abc8e34039ac67f583ceabf3b5952
Shamanbenny <E1186165@u.nus.edu>
2024-06-08 23:44:16 +0800

    Fixed & Finalized Problem 8: String to Integer (atoi)

--- 8_String to Integer_atoi/solution.py
+++ 8_String to Integer_atoi/solution.py
@@ -1,7 +1,19 @@
+# Solution Info
+# Runtime: 24 ms, faster than 99.31% of Python3 submissions.
+# Memory: 16.47 MB, less than 94.27% of Python3 submissions.
 class Solution:
     def myAtoi(self, s: str) -> int:
+        def boundCheck(x: int) -> int:
+            if x < -2147483648:
+                return -2147483648
+            elif x > 2147483647:
+                return 2147483647
+            else:
+                return x
+
         try:
-            return int(s)
+            result = int(s)
+            return boundCheck(result)
         except ValueError:
             leading_whitespace = 0
             sign = 0  # 1 for positive, -1 for negative
@@ -16,23 +28,31 @@ class Solution:
                 if state == 1:
                     if char == "-":
                         sign = -1
+                        state = 2
+                        continue
                     elif char == "+":
                         sign = 1
-                    else:
                         state = 2
+                        continue
+                    state = 2
                 if state == 2:
                     if char.isdigit():
                         int_length += 1
                     else:
                         break
             if int_length > 0:
+                result = 0
                 if sign == 0:
-                    return int(s[leading_whitespace : leading_whitespace + int_length])
+                    result = int(
+                        s[leading_whitespace : leading_whitespace + int_length]
+                    )
                 else:
-                    return sign * int(
+                    result = sign * int(
                         s[leading_whitespace + 1 : leading_whitespace + int_length + 1]
                     )
+                return boundCheck(result)
+            return 0
 
 
 solution = Solution()
-print(solution.myAtoi("42") == -42)  # -42
+print(solution.myAtoi("  -0012a42"))
